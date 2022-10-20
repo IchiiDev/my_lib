755bf61027376e2a4824d36a4d7ed4bd9cf0890e
Ethan Thierry <ethan.thierry@epitech.eu>
2022-10-20 13:25:56 +0200

    Push my_compute_square_root

--- lib/my/my_compute_square_root.c
+++ lib/my/my_compute_square_root.c
@@ -7,5 +7,11 @@
 
 int my_compute_square_root(int nb)
 {
+    int i = 0;
+    while (i * i < 2147483647) {
+        if (i * i > nb) return 0;
+        if (i * i == nb) return i;
+        i += 1;
+    }
     return 0;
 }
