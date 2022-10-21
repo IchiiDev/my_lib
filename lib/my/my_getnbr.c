9914b9e1f271ca13cac8ccdff2495c66277f5b39
Ethan Thierry <ethan.thierry@epitech.eu>
2022-10-21 11:30:10 +0200

    Implenting my_getnbr

--- lib/my/my_getnbr.c
+++ lib/my/my_getnbr.c
@@ -12,14 +12,16 @@ int my_getnbr(char const *str)
     long count = 0;
     int is_minus = 1;
     for (int i = 0; str[i] != '\0'; i++) {
-        if (!IS_NBR(str[i]) && count > 0) {
+        if ((!IS_NBR(str[i])) && count > 0) {
             return count * is_minus;
         }
         if (count > 2147483648) {
             return 0;
         }
         if (IS_NBR(str[i])) {
+            if (str[i - 1] == '-') is_minus = -1;
             count = count * 10 + (str[i] - '0');
         }
     }
+    return count * is_minus;
 }
